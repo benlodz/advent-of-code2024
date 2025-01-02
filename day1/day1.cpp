6309a9c5287658a0766840ac999faa73dac47c96
Benjamin Lodzhevsky <ben@benlodz.com>
2025-01-01 19:43:05 -0500

    cleaned up git related stuff and edits for day1.cpp

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -1,81 +1,91 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <vector>
-#include <map>
-#include <queue>
+#include <common/common.hpp>
 
-int main() {
+std::pair<std::vector<s32>, std::vector<s32>> getLists(
+    std::vector<std::string> &lines) {
+  // In order to solve day 1's problems, we need to split the input file s32o
+  // two separate lists. Our lists are just numbers so we can go ahead and
+  // create an array of numbers for every list.
 
-    // everything used is from standard library, so save the time
-    using namespace std;
-    // load file
-    ifstream file;
-    vector<string> lines;
-    string line;
+  std::vector<s32> l1(lines.size());
+  std::vector<s32> l2(lines.size());
 
-    // error handling
-    file.exceptions(ifstream::badbit | ifstream::failbit);
+  s32 v1, v2;
 
-    try {
-        file.open("day1_input.txt");
-        while (getline(file, line)) {
-            lines.push_back(line);
-        }
-    } catch (ifstream::failure e) {
-        runtime_error("Failed to open file!");
+  for (const auto &line : lines) {
+    std::istringstream iss(line);
+    // >> stops at whitespace
+    if (!(iss >> v1 >> v2)) {
+      std::runtime_error("Failed to extract characters!");
+    } else {
+      l1.push_back(v1);
+      l2.push_back(v2);
     }
+  }
 
-    vector<int> l1(lines.size());
-    vector<int> l2(lines.size());
+  std::pair<std::vector<s32>, std::vector<s32>> lists(l1, l2);
+  return lists;
+}
 
-    string d1_s, d2_s;
+s32 getDistance(const std::pair<std::vector<s32>, std::vector<s32>> &lists) {
+  // In the first part of the problem, we need to take the smallest possible
+  // number from both lists, and then calculate the distance between them.
+  //
+  // Example:
+  // 3   4
+  // 4   3
+  // 2   5
+  // 1   3
+  // 3   9
+  // 3   3
+  // In this scenario, we would take 1 from the first list and take 3 from the
+  // second. The distance between them would be |1 - 3| = 2. We then repeat
+  // this process. This is easily done by using a min-heap and then popping
+  // from both of them.
 
-    for (const auto& line: lines) {
-        istringstream iss(line);
-        getline(iss, d1_s, ' ');
-        getline(iss, d2_s);
+  std::priority_queue<s32, std::vector<s32>, std::greater<s32>> h1;
+  std::priority_queue<s32, std::vector<s32>, std::greater<s32>> h2;
 
-        l1.push_back(stoi(d1_s));
-        l2.push_back(stoi(d2_s));
-    }
+  for (const auto &n : lists.first) {
+    h1.push(n);
+  }
 
-    // first we'll get the result of part 2 since we need to convert
-    // them into heaps in part 1
+  for (const auto &n : lists.second) {
+    h2.push(n);
+  }
 
-    map<int, int> cnt;
-    for (const auto& d2: l2) {
-        cnt[d2]++;
-    }
+  s32 distance{};
+  // They're the same size so doesn't matter which one.
+  while (!h1.empty()) {
+    distance += abs(h1.top() - h2.top());
+    h1.pop();
+    h2.pop();
+  }
+  return distance;
+}
 
-    long long int similarity_score {}; 
-    for (const auto& d1: l1) {
-        similarity_score += d1 * cnt[d1];
-    }
+s32 getSimilarity(std::pair<std::vector<s32>, std::vector<s32>> &lists) {
+  // For part 2 of day 1, you have to calculate a "similarity score".
+  // Essentially, for every number in the first list, how many times does it
+  // occur in the second? We can accomplish this pretty easily with a hashmap.
+  std::map<s32, s32> cnt;
+  for (const auto n2 : lists.second) {
+    cnt[n2] += 1;
+  }
+  s32 score{};
+  for (const auto n1 : lists.first) {
+    score += cnt[n1];
+  }
+  return score;
+}
 
-    priority_queue<int, vector<int>, greater<int>> min_heap1;
-    priority_queue<int, vector<int>, greater<int>> min_heap2;
+s32 main(int argc, char *argv[]) {
+  auto file_path = parse_arguments(
+      "day1", "This program solves day 1 problem for the Advent of Code 2024.",
+      argc, argv);
+  auto lines = getLines(file_path.c_str());
+  auto lists = getLists(lines);
 
-    // push our vectors into the heaps
-    for (const auto& n: l1) {
-        min_heap1.push(n);
-    }
-
-    for (const auto& n: l2) {
-        min_heap2.push(n);
-    }
-
-    long long int distance {};
-    while (!min_heap1.empty()) {
-        distance += abs(min_heap1.top() - min_heap2.top());
-        min_heap1.pop();
-        min_heap2.pop();
-    }
-
-    cout << "Distance: " << distance << endl;
-    cout << "Similarity Score: " << similarity_score << endl;
-    
-    return 0;
+  auto distance = getDistance(lists);
+  auto similarity_score = getSimilarity(lists);
+  return 0;
 }
